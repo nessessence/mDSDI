0ceff19e6cbac717086fd4fa064cc676a453d806
manhhabui <hb.buimanhha@gmail.com>
2021-02-10 10:53:18 +0000

    edit data path

--- download_script.sh
+++ download_script.sh
@@ -1,8 +1,13 @@
+#Download pretrained models
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bDey1GKP528wWfuLqGev5RBIUbHPoOxO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1bDey1GKP528wWfuLqGev5RBIUbHPoOxO" -O pretrained_models.zip && rm -rf /tmp/cookies.txt
+unzip pretrained_models.zip
+
+#Download dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1W_6aYi_DR_02j0Z01LsL1rCe7OS5B0pE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1W_6aYi_DR_02j0Z01LsL1rCe7OS5B0pE" -O data.zip && rm -rf /tmp/cookies.txt
 unzip data.zip
 cd data/DomainNet/Raw\ images/
 
-#Download DomainNet Dataset
+#Download DomainNet dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1fpD2L6lsejDD3rWLOGeqnlt5pplZEYR5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1fpD2L6lsejDD3rWLOGeqnlt5pplZEYR5" -O clipart.zip && rm -rf /tmp/cookies.txt
 unzip clipart.zip
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ypzUY4BHfTz4EnPk6JwwTYQPGpHdV0Ts' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ypzUY4BHfTz4EnPk6JwwTYQPGpHdV0Ts" -O infograph.zip && rm -rf /tmp/cookies.txt
