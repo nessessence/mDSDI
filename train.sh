a9bc0d6f59f384baf16e194b8e6f7fe77133eeff
manhhabui <hb.buimanhha@gmail.com>
2021-02-11 06:47:38 +0000

    update README.md

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
-# for i in {1..1}; do
+# for i in {1..5}; do
 #      taskset -c "51" python main.py --config "algorithms/mDSDI/configs/PACS_photo.json" --exp_idx $i --gpu_idx "1"
 # done
 
-# tensorboard --logdir "/home/ubuntu/DSDI/algorithms/mDSDI/results/tensorboards/PACS_photo_1"
+# tensorboard --logdir "/home/ubuntu/mDSDI/algorithms/mDSDI/results/tensorboards/PACS_photo_1"
 # python utils/tSNE_plot.py --plotdir "algorithms/mDSDI/results/plots/PACS_photo_1/"
 
 # rm -r algorithms/mDSDI/results/checkpoints/*
